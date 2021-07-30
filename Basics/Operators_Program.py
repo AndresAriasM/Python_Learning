14a34e9cd4b19d31044acbc7a6f5f3a2a0ed61db
Andrés Arias Medina <77759820+AndresAriasM@users.noreply.github.com>
2021-07-30 00:27:02 -0500

    Create Operators_Program.py

---
+++ Basics/Operators_Program.py
@@ -0,0 +1,7 @@
+num1 = float(input ("Enter a whole number between 1 and 10"))
+num2 = float(input ("Enter a whole number between 1 and 10 again"))
+
+print ("Are the numbers the same? " , num1 == num2)
+print ("Are the numbers different? " , num1 != num2)
+print ("The first number is bigger than the second number? " , num1 > num2)
+print ("The second number is bigger than the second number? " , num2 > num1)

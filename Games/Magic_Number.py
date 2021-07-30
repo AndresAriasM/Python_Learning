096f86137beb6dab0d940d696f3bb546df69b2c0
Andrés Arias Medina <77759820+AndresAriasM@users.noreply.github.com>
2021-07-30 01:28:52 -0500

    Create Magic_Number.py

---
+++ Games/Magic_Number.py
@@ -0,0 +1,5 @@
+magic_number = 12345679
+user_number = int(input ("Write down a number from 1 to 9"))
+magic_number *= 9 
+magic_number *= user_number
+print ("The magic number is: " , magic_number)
